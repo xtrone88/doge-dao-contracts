75118bd1a8d1482e1b11602be35e53c037c5535f
zess11 <zess11lin@gmail.com>
2021-08-16 17:04:14 -0400

    updated testing lge

--- test/TestLgeContract.js
+++ test/TestLgeContract.js
@@ -12,14 +12,31 @@ contract('TestLgeContract', (accounts) => {
     });
 
     it ("Test contribution to LGE", async () => {
+        let expectedTotal = 0;
         let account0_contribution = await dfmContract.contributionOf(accounts[0]);
         account0_contribution = account0_contribution.toString();
         console.log("Balance of Account[0]", account0_contribution);
 
+        expectedTotal += 125;
         await dfmContract.contribute({value:web3.utils.toWei('1.25', 'ether'),from:accounts[0]});
 
         account0_contribution = await dfmContract.contributionOf(accounts[0]);
         account0_contribution = account0_contribution.toString();
         console.log("Balance of Account[0]", account0_contribution);
+
+        let account1_contribution = await dfmContract.contributionOf(accounts[1]);
+        account1_contribution = account1_contribution.toString();
+        console.log("Balance of Account[1]", account1_contribution);
+
+        expectedTotal += 250;
+        await dfmContract.contribute({value:web3.utils.toWei('2.5', 'ether'),from:accounts[1]});
+
+        account1_contribution = await dfmContract.contributionOf(accounts[1]);
+        account1_contribution = account1_contribution.toString();
+        console.log("Balance of Account[1]", account1_contribution);
+
+        expectedTotal = BN(expectedTotal).mul(BN(10).pow(BN(18))).div(BN(100));
+        let totalContribution = await dfmContract.totalContirbuted();
+        assert(expectedTotal, totalContribution.toString(), "Total Contribution isn't correct");
     });
 });
